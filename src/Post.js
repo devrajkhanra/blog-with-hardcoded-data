dc883069765c944e67f52ae611866af302c6b8fe
riku.devd <riku.devd@gmail.com>
2022-11-19 19:28:08 +0530

    Y

--- src/Post.js
+++ src/Post.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Post = ({ post }) => {
     return (
-        <>
-            <h3 className='font-thin text-2xl'>{post.title}</h3>
-            <p className='font-thin'>{post.body}</p>
-            <br />
-        </>
+        <div className='flex flex-col mx-3 bg-white my-4 px-2'>
+            <Link to={`/post/${post.id}`}>
+                <h3 className='font-normal text-2xl text-gray-600'>{post.title}</h3>
+                <p className='font-thin text-sm text-gray-900'>{post.datetime}</p>
+            </Link>
+            <hr />
+            <p className='font-thin text-xl text-gray-600'>{
+                (post.body).length <= 25 ? post.body : `${(post.body).slice(0, 25)} ...`}
+            </p>
+        </div>
     )
 }
 
