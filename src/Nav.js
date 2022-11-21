81436b35ee491f3da6db3647bb1a4589103f85d1
riku.devd <riku.devd@gmail.com>
2022-11-21 18:14:52 +0530

    y

--- src/Nav.js
+++ src/Nav.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import { MdHome, MdPostAdd } from 'react-icons/md'
 
 const Nav = ({ search, setSearch }) => {
+
     return (
         <nav className='flex justify-center items-center bg-gray-800'>
             <form onSubmit={(e) => e.preventDefault()}>
@@ -15,9 +16,9 @@ const Nav = ({ search, setSearch }) => {
                     onChange={(e) => setSearch(e.target.value)} />
             </form>
             <ul className=' text-gray-500 flex justify-evenly items-center'>
-                <li className=' px-3 hover:text-sky-600 hover:bg-purple-200 py-3 text-2xl hover:rounded-full'><Link to='/'><MdHome /></Link></li>
-                <li className='px-3 hover:text-sky-600 hover:bg-purple-200 py-3 text-2xl hover:rounded-full'><Link to='/post'><MdPostAdd /></Link></li>
-                <li className='font-thin px-3 hover:text-sky-900 hover:font-bold hover:bg-purple-200 py-3 hover:rounded-full'><Link to='/about'>About</Link></li>
+                <li className=' px-3 hover:text-sky-600 hover:bg-purple-200 py-3 text-2xl'><Link to='/'><MdHome /></Link></li>
+                <li className='px-3 hover:text-sky-600 hover:bg-purple-200 py-3 text-2xl'><Link to='/post'><MdPostAdd /></Link></li>
+                <li className='font-thin px-3 hover:text-sky-900 hover:font-bold hover:bg-purple-200 py-3'><Link to='/about'>About</Link></li>
             </ul>
         </nav>
     )
